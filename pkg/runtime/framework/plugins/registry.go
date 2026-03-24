4e0854865a964a250b9626b6fc57044f7d14e839
Erika Sternad <erika.sternad@csc.fi>
2026-03-24 07:10:35 +0000

    Merge upstream/master into lumi-remote and resolve conflicts

--- pkg/runtime/framework/plugins/registry.go
+++ pkg/runtime/framework/plugins/registry.go
@@ -51,6 +51,8 @@ func NewRegistry() Registry {
 		jax.Name:          jax.New,
 		xgboost.Name:      xgboost.New,
 		remote.Name:       remote.New, // new remote plugin
+		jax.Name:          jax.New,
+		xgboost.Name:      xgboost.New,
 	}
 
 	if features.Enabled(features.TrainJobStatus) {
