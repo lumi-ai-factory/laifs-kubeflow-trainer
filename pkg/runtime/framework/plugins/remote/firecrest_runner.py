d7ae980bb5270ce731b7114bd0de8ba3748bcb46
Erika Sternad <erika.sternad@csc.fi>
2026-05-26 13:39:42 +0300

    Revert back to previous working dir logic, removed timestamp from dir name

--- pkg/runtime/framework/plugins/remote/firecrest_runner.py
+++ pkg/runtime/framework/plugins/remote/firecrest_runner.py
@@ -216,7 +216,7 @@ def main():
         date_dir = now.strftime("%Y-%m-%d")
         day_path = f"{remote_path}/kf_output/{date_dir}"
 
-        run_id = now.strftime("%H-%M-%S") + "_" + uuid.uuid4().hex[:6]
+        run_id = uuid.uuid4().hex[:6]
         job_dir = f"{day_path}/{run_id}"
 
         try:
@@ -280,24 +280,6 @@ def main():
             sys.exit(1)
 
         print(f"Submitted jobid={jobid}")
-
-        print(f"Submitted jobid={jobid}")
-
-        final_job_dir = f"{day_path}/{jobid}"
-
-        print(f"Renaming remote directory to {final_job_dir}")
-
-        try:
-            client.mv(
-                machine,
-                source_path=job_dir,
-                target_path=final_job_dir,
-            )
-
-            job_dir = final_job_dir
-
-        except Exception as e:
-            print(f"WARNING: failed to rename remote directory: {e}")
 
         patch_trainjob_annotation(jobid)
 
